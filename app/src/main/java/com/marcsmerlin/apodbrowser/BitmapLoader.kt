19abb44d5b169eaff1dd8c5f5a82f036a316d140
Marc Merlin <marc.s.merlin@gmail.com>
2021-02-26 08:01:47 -0500

    Factored IBitmapLoader into standalone file.

--- app/src/main/java/com/marcsmerlin/apodbrowser/BitmapLoader.kt
+++ app/src/main/java/com/marcsmerlin/apodbrowser/BitmapLoader.kt
@@ -1,19 +1,8 @@
 package com.marcsmerlin.apodbrowser
 
-import android.graphics.Bitmap
 import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
-
-sealed class BitmapStatus {
-    object Loading : BitmapStatus()
-    data class Success(val bitmap: Bitmap) : BitmapStatus()
-    data class Error(val error: Exception) : BitmapStatus()
-}
-
-interface IBitmapLoader {
-    fun queueRequest(url: String): State<BitmapStatus>
-}
 
 class BitmapLoader(
     private val queue: IBitmapQueue
---
+++ app/src/main/java/com/marcsmerlin/apodbrowser/IBitmapLoader.kt
@@ -0,0 +1,14 @@
+package com.marcsmerlin.apodbrowser
+
+import android.graphics.Bitmap
+import androidx.compose.runtime.State
+
+sealed class BitmapStatus {
+    object Loading : BitmapStatus()
+    data class Success(val bitmap: Bitmap) : BitmapStatus()
+    data class Error(val error: Exception) : BitmapStatus()
+}
+
+interface IBitmapLoader {
+    fun queueRequest(url: String): State<BitmapStatus>
+}
