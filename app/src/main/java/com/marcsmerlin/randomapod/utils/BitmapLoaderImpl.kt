f9f715e0ce35c78c31adcd9a9c52878382355c5a
Marc Merlin <marc.s.merlin@gmail.com>
2021-03-11 00:27:32 -0500

    Implemented single-level cache for BitmapLoaderImpl to reduce redundant downloads dur to screen recompositions.

--- app/src/main/java/com/marcsmerlin/randomapod/utils/BitmapLoaderImpl.kt
+++ app/src/main/java/com/marcsmerlin/randomapod/utils/BitmapLoaderImpl.kt
@@ -1,5 +1,6 @@
 package com.marcsmerlin.randomapod.utils
 
+import android.graphics.Bitmap
 import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
@@ -8,35 +9,50 @@ class BitmapLoaderImpl(
     private val requestQueue: BitmapRequestQueue
 ) : BitmapLoader {
 
+    private object cache {
+        var url: String? = null
+        lateinit var bitmap: Bitmap
+    }
+
     override fun queueRequest(url: String): State<BitmapLoader.Status> {
-
-        val tag = this::class.java
-
-        Log.i("$tag", "Bitmap request queued for: $url")
 
         val result = mutableStateOf<BitmapLoader.Status>(
             BitmapLoader.Status.Loading(url = url)
         )
 
-        requestQueue.addBitmapRequest(
-            url = url,
-            { bitmap ->
-                result.value =
-                    BitmapLoader.Status.Done(
-                        url = url,
-                        bitmap = bitmap
-                    )
+        if (cache.url != null && cache.url == url) {
+            result.value =
+                BitmapLoader.Status.Done(
+                    url = url,
+                    bitmap = cache.bitmap
+                )
+        } else {
+            val tag = this::class.java
+            Log.i("$tag", "Queuing bitmap request for: $url")
 
-                Log.i("$tag", "Bitmap received for: $url")
-            },
-            { exception ->
-                result.value =
-                    BitmapLoader.Status.Failed(
-                        url = url,
-                        error = exception
-                    )
-            },
-        )
+            requestQueue.addBitmapRequest(
+                url = url,
+                { bitmap ->
+                    result.value =
+                        BitmapLoader.Status.Done(
+                            url = url,
+                            bitmap = bitmap
+                        )
+
+                    Log.i("$tag", "Bitmap received for: $url")
+
+                    cache.url = url
+                    cache.bitmap = bitmap
+                },
+                { exception ->
+                    result.value =
+                        BitmapLoader.Status.Failed(
+                            url = url,
+                            error = exception
+                        )
+                },
+            )
+        }
 
         return result
     }
